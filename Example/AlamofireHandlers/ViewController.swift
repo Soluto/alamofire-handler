d88b185ec55e97ea61ffab07c05d20ac518e7b5c
Or Rosenblatt <orr@soluto.com>
2016-07-25 13:12:21 +0300

    Fix warning of not using the result of subscribeNext

--- Example/AlamofireHandlers/ViewController.swift
+++ Example/AlamofireHandlers/ViewController.swift
@@ -7,8 +7,10 @@
 //
 import UIKit
 import AlamofireHandlers
+import RxSwift
 
 class ViewController: UIViewController {
+    private let bag : DisposeBag = { return DisposeBag() }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,13 +19,11 @@ class ViewController: UIViewController {
         let manager = DelegatingManager(handler: AlamofireHandler())
         manager.request(.GET, "https://httpbin.org/headers").subscribeNext { result in
             print(result.serializeString())
-        }
+        }.addDisposableTo(bag)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
-
